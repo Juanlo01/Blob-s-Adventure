c60fb88b4af809a1f75eac903faed83805015f9b
Juanlo01 <jcota3264@gmail.com>
2024-11-04 21:26:10 -0800

    Code Cleanup
    Cleaned up the code a bit (Still a LOT unused)

--- Blob's Adventure/Assets/Scripts/DeathBarrier.cs
+++ Blob's Adventure/Assets/Scripts/DeathBarrier.cs
@@ -5,16 +5,10 @@ using UnityEngine;
 public class DeathBarrier : MonoBehaviour
 {
 
-    AudioSource Death;
-
-    void Awake(){
-        Death = GetComponent<AudioSource>();    
-    }
+   
 
     void OnCollisionEnter2D(Collision2D collision){
-        if (collision.gameObject.CompareTag("Player")){
-            Death.Play();
-        }
+        
 
         if (collision.gameObject.CompareTag("Player")){
                 int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
